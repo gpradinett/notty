768c85ccf2812c7e6cff70e7ec50420eb3aef46e
gpradinett <gpradinett@gmail.com>
2025-02-17 02:49:47 -0500

    ✅ Added upload banner and function to paste and delete the inserted link.

--- file.py
+++ file.py
@@ -5,6 +5,7 @@ import re
 import os
 from fastapi import HTTPException
 
+
 DOWNLOAD_PATH = "./downloads"
 if not os.path.exists(DOWNLOAD_PATH):
     os.makedirs(DOWNLOAD_PATH)
@@ -28,10 +29,7 @@ class Downloader(ABC):
         """
         print("Operación común a todos los descargadores")
         product = self.factory_method(url)
-        result = f"Resultado: {product.operation()}"
-        print("Operación común a todos los descargadores0" , result)
-        result1 = product.operation()
-        print("Operación común a todos los descargadores1", result1)
+        result = product.operation()
         return result
     
     
@@ -60,6 +58,7 @@ class Product(ABC):
         """
         pass
 
+
 class TikTokDownloader(Downloader):
     """
     Descargador concreto que descarga videos de TikTok.
@@ -76,48 +75,29 @@ class TikTokProduct(Product):
     def __init__(self, url: str):
         self.url = url
     
-    def operation(self) -> str:
-        """
-        Lógica para descargar el video de TikTok.
-        """
+    def download_video(self) -> str:
         with yt_dlp.YoutubeDL({"noplaylist": True}) as ydl:
             info_dict = ydl.extract_info(self.url, download=False)
             video_filename = info_dict.get("title", "TikTok Video")
             clean_video_title = TikTokDownloader().sanitize_filename(video_filename)
 
-        # Ruta para el archivo descargado
         video_file_path = os.path.join(DOWNLOAD_PATH, f"{clean_video_title}.mp4")
-        
-        # Verificar si el archivo ya existe
-        if os.path.exists(video_file_path):
-            print(f"El archivo ya existe: {video_file_path}")
-            return video_file_path  # Retorna el archivo existente
 
-        # Configuración de yt-dlp para la descarga
-        ydl_opts = {
-            "format": "best",
-            "outtmpl": video_file_path,
-            "geo_bypass": True,
-        }
-
-        # Descargar el video
+        ydl_opts = {"format": "best", "outtmpl": video_file_path, "geo_bypass": True}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([self.url])
 
-        # Verificar si el archivo fue descargado correctamente
         if not os.path.exists(video_file_path):
-            raise HTTPException(status_code=500, detail="El archivo no fue descargado correctamente.")
-
+            raise DownloadError("El archivo no fue descargado correctamente.")
         return video_file_path
 
+    def operation(self) -> str:
+        return self.download_video()
 
-def client_code(downloader: Downloader, url: str) -> None:
-    """
-    El client code trabaja con una instancia de un creador concreto, aunque lo hace a través de su interfaz base. Siempre que el cliente siga trabajando con el creador mediante la interfaz base, puedes pasarle cualquier subclase del creador..
-    """
 
-    print(f"Cliente: No estoy al tanto de la clase del creador, pero aún así funciona\n"
-          f"{downloader.some_operation(url)}")
+class DownloadError(HTTPException):
+    def __init__(self, message: str):
+        super().__init__(status_code=500, detail=message)
 
 
 def get_downloader(url: str):
